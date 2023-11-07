5d811bb842a28b55ee1b1a5c959a7c8c210783b0
ivanmorikov <ivanmorikov48@gmail.com>
2023-11-07 18:56:09 +0300

    added several different values of k and a model where features are randomly selected

--- k-NN/main.py
+++ k-NN/main.py
@@ -1,6 +1,8 @@
+import random
 import numpy as np
 import pandas as pd
 import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
 
 df = pd.read_csv("WineDataset.csv")
 print(df.describe())
@@ -11,23 +13,74 @@ df = df.fillna(df.mean)
 
 missing_data = df.isnull()
 
+selected_columns = random.sample(range(len(df.columns) - 1), k=random.randint(0, df.shape[1] - 1))
+selected_columns.append(len(df.columns) - 1)
+random_df = df.iloc[:, selected_columns]
+
 X = df[['Alcohol', 'Malic Acid', 'Ash', 'Alcalinity of ash', 'Magnesium',
         'Total phenols', 'Flavanoids', 'Nonflavanoid phenols', 'Proanthocyanins',
         'Color intensity', 'Hue', 'OD280/OD315 of diluted wines', 'Proline']].to_numpy()
 y = df[['Wine']].to_numpy()
+X_random = df.drop(columns=['Wine']).to_numpy()
+
 
 indices = np.arange(len(X))
+indices1 = np.arange(len(X_random))
 np.random.shuffle(indices)
+np.random.shuffle(indices1)
 test_size = 0.2
 split_index = int(len(X) * (1 - test_size))
+split_index1 = int(len(X_random) * (1 - test_size))
 
+X_train_ran, X_test_ran = X_random[indices1[:split_index1]], X_random[indices1[split_index1:]]
 X_train, X_test = X[indices[:split_index]], X[indices[split_index:]]
 y_train, y_test = y[indices[:split_index]], y[indices[split_index:]]
 
 model = KNeighborsClassifier.KNeighborsClassifier(3, 3)
 model.fit(X_train, y_train, X_test)
-pred = model.predict(6, y_test)
-print(pred)
+
+pred = model.predict(3, y_test)
 accuracy_score = model.accuracy(y_test, pred)
-print(y_test)
+print("k = 3")
+print(confusion_matrix(y_test, pred))
 print(accuracy_score)
+print("***************")
+
+pred = model.predict(5, y_test)
+accuracy_score = model.accuracy(y_test, pred)
+print("k = 5")
+print(confusion_matrix(y_test, pred))
+print(accuracy_score)
+print("***************")
+
+pred = model.predict(10, y_test)
+accuracy_score = model.accuracy(y_test, pred)
+print("k = 10")
+print(confusion_matrix(y_test, pred))
+print(accuracy_score)
+print("***************")
+
+model_random = KNeighborsClassifier.KNeighborsClassifier(3, 3)
+model_random.fit(X_train_ran, y_train, X_test_ran)
+
+pred = model.predict(3, y_test)
+accuracy_score = model.accuracy(y_test, pred)
+print("k = 3, random")
+print(confusion_matrix(y_test, pred))
+print(accuracy_score)
+print("***************")
+
+pred = model.predict(5, y_test)
+accuracy_score = model.accuracy(y_test, pred)
+print("k = 3, random")
+print(confusion_matrix(y_test, pred))
+print(accuracy_score)
+print("***************")
+
+pred = model.predict(10, y_test)
+accuracy_score = model.accuracy(y_test, pred)
+print("k = 3, random")
+print(confusion_matrix(y_test, pred))
+print(accuracy_score)
+print("***************")
+
